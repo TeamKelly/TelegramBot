cffe23bdf8889c913c6f6050c4e6d9092f7f51df
yeongjinoh <oyj9109@snu.ac.kr>
2017-10-13 19:29:34 +0900

    basic setting for database

---
+++ main/database.py
@@ -0,0 +1,75 @@
+import json
+import random
+from firebase import firebase
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+firebase = firebase.FirebaseApplication(config['DATABASE']['USER'], None)
+
+def init_colors():
+    colors = {
+        1:{'r':255, 'g':0, 'b':0},
+        2:{'r':127, 'g':127, 'b':0},
+        3:{'r':0, 'g':255, 'b':0},
+        4:{'r':0, 'g':127, 'b':127},
+        5:{'r':0, 'g':0, 'b':255},
+        6:{'r':127, 'g':0, 'b':127}}
+    result = firebase.post('/colors', colors)
+    print result
+
+def add_new_user(username):
+    new_user = {'name':username, 'mode':1}
+    result = firebase.post('/users', new_user)
+    print result
+
+def get_user(username):
+    url = '/users/' + config['DATABASE'][username]
+    result = firebase.get(url, None)
+    print result
+
+def get_url(username):
+    return '/users/' + config['DATABASE'][username]
+
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+def get_date_with_random_emotions(month, date):
+    emotions = []
+    for i in range(24):
+        emotions.append(random.randint(1,6))
+    return {
+        'month':month,
+        'date':date,
+        'emotions':emotions,
+        'rep_emotion':most_common(emotions)
+    }
+
+def get_dates_with_random_emotions(month, num_of_dates):
+    dates = []
+    for date in range(1,num_of_dates+1):
+        dates.append(get_date_with_random_emotions(month,date))
+    return dates
+
+def update_mode(username, mode):
+    url = get_url(username)
+    result = firebase.put(url, 'mode', mode)
+    print "update_mode"
+    print result
+
+def update_dates(username, dates=None):
+    if dates is None:
+        dates = get_dates_with_random_emotions(10, 13)
+    url = get_url(username)
+    result = firebase.put(url, 'dates', dates)
+    print "update_dates"
+    print result
+
+def main():
+    #init_colors()
+    #update_mode(username, 1)
+    username = 'yeongjin'
+    update_dates(username)
+
+if __name__ == '__main__':
+    main()
